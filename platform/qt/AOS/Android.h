0edca18b7d9066978ecfb26cafa2d8ebe5d944ba
RIP-tommy <sungminchoidev@gmail.com>
2024-12-25 13:51:00 +0900

    refact: split android related files from main.cpp
    and add some lines some function just works in android

--- platform/qt/AOS/Android.h
+++ platform/qt/AOS/Android.h
@@ -1,22 +1,4 @@
 #ifndef ANDROID_H
 #define ANDROID_H
-
-#include "../../src/mlv_include.h"
-
-#include <QFile>
-#include <QDebug>
-#include <QString>
-
-QString createFolderInAndroidUri(const QString &parentUri, const QString &folderName);
-qint8 runExport(const QString &inputFile, const QString &outputFile);
-bool checkFFmpeg();
-bool runFFmpegCmd(QString cmd, QString outputFile);
-QString getFFMpegPipe();
-bool runFFmpegCmdInPipe(QString tmpImgPath, QString cmd, QString pipe);
-void closeFFmpegPipe(QString pipe);
 void requestAllFilesAccess();
-void triggerBrightWakeLock();
-void releaseWakeLock();
-void triggerDimWakeLock();
-
 #endif // ANDROID_H
